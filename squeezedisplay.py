d91afa3575e3ede174f55f48d1aeb81411b76d6f
Martin Grohmann <martin@medi-inf.org>
2012-12-06 09:25:01 +0100

    added CC BY SA License

--- squeezedisplay.py
+++ squeezedisplay.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# rpi-squeezedisplay: A small Tool to display Information on a display 
+# attached to the Raspberry Pi
+# Copyright (C) 2012 Martin Grohmann
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Creative Commomns - Share Alike License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Creative Commons - Share Alike License for more details.
+# 
+# You should have received a copy of the License along with this program. 
+# If not, see <https://creativecommons.org/licenses/by-sa/3.0/>
+
 from pylms.server import Server
 from pylms.player import Player
 import subprocess
